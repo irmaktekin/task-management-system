83839f1a45340aed231f2dabd1cb9042f016f42d
irmaktekin <irmaktekin97@gmail.com>
2025-03-10 23:21:05 +0300

    Add createUser method to UserService interface

--- src/main/java/com/irmaktekin/task/management/system/service/UserService.java
+++ src/main/java/com/irmaktekin/task/management/system/service/UserService.java
@@ -1,4 +1,7 @@
 package com.irmaktekin.task.management.system.service;
 
+import com.irmaktekin.task.management.system.entity.User;
+
 public interface UserService {
+    User createUser(User user);
 }
