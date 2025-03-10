9a231529b338b4085141a55c586e3bd4cb5a9528
irmaktekin <irmaktekin97@gmail.com>
2025-03-10 23:03:36 +0300

    Create UserService class with empty methods for TDD

---
+++ src/main/java/com/irmaktekin/task/management/system/service/UserService.java
@@ -0,0 +1,4 @@
+package com.irmaktekin.task.management.system.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/irmaktekin/task/management/system/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.irmaktekin.task.management.system.service.impl;
+
+import com.irmaktekin.task.management.system.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
