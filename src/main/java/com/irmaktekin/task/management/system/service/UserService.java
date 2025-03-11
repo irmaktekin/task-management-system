4c468eedde79d52f18e1ec80a9342b9285d2ed08
irmaktekin <irmaktekin97@gmail.com>
2025-03-11 23:28:10 +0300

    UserService interface is implemented

--- src/main/java/com/irmaktekin/task/management/system/service/UserService.java
+++ src/main/java/com/irmaktekin/task/management/system/service/UserService.java
@@ -1,7 +1,15 @@
 package com.irmaktekin.task.management.system.service;
 
+import com.irmaktekin.task.management.system.common.exception.UserNotFoundException;
 import com.irmaktekin.task.management.system.entity.User;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface UserService {
     User createUser(User user);
+    User updateUser(UUID id, User user) throws UserNotFoundException;
+    List<User> getUsers();
+    User findUserById(UUID id) throws UserNotFoundException;
+    void deleteUser(UUID id);
 }
--- src/main/java/com/irmaktekin/task/management/system/service/impl/UserServiceImpl.java
+++ src/main/java/com/irmaktekin/task/management/system/service/impl/UserServiceImpl.java
@@ -1,8 +1,38 @@
 package com.irmaktekin.task.management.system.service.impl;
 
+import com.irmaktekin.task.management.system.common.exception.UserNotFoundException;
+import com.irmaktekin.task.management.system.entity.User;
 import com.irmaktekin.task.management.system.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Override
+    public User createUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(UUID id, User user) throws UserNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return List.of();
+    }
+
+    @Override
+    public User findUserById(UUID id) throws UserNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(UUID id) {
+
+    }
 }
