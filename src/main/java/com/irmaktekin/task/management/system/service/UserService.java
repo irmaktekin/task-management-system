1369f74e3b3a11d65bc12d81d68f073001142ad6
irmaktekin <irmaktekin97@gmail.com>
2025-03-13 23:39:31 +0300

    Add Lombok annotation processor to fix builder error

--- src/main/java/com/irmaktekin/task/management/system/service/UserService.java
+++ src/main/java/com/irmaktekin/task/management/system/service/UserService.java
@@ -1,15 +1,16 @@
 package com.irmaktekin.task.management.system.service;
 
 import com.irmaktekin.task.management.system.common.exception.UserNotFoundException;
+import com.irmaktekin.task.management.system.dto.request.UserCreateRequest;
+import com.irmaktekin.task.management.system.dto.response.UserDto;
 import com.irmaktekin.task.management.system.entity.User;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 import java.util.UUID;
 
 public interface UserService {
-    User createUser(User user);
+    User createUser(UserCreateRequest userCreateRequest);
     User updateUser(UUID id, User user) throws UserNotFoundException;
-    List<User> getUsers();
+    Page<UserDto> getUsers(int page, int size);
     User findUserById(UUID id) throws UserNotFoundException;
     void deleteUser(UUID id);
 }
--- src/main/java/com/irmaktekin/task/management/system/service/impl/UserServiceImpl.java
+++ src/main/java/com/irmaktekin/task/management/system/service/impl/UserServiceImpl.java
@@ -1,38 +1,64 @@
 package com.irmaktekin.task.management.system.service.impl;
 
 import com.irmaktekin.task.management.system.common.exception.UserNotFoundException;
+import com.irmaktekin.task.management.system.dto.request.UserCreateRequest;
+import com.irmaktekin.task.management.system.dto.response.UserDto;
 import com.irmaktekin.task.management.system.entity.User;
+import com.irmaktekin.task.management.system.repository.UserRepository;
 import com.irmaktekin.task.management.system.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
-    public User createUser(User user) {
-        return null;
+    public User createUser(@Valid UserCreateRequest userCreateRequest) {
+        User createdUser = User.builder().fullName(userCreateRequest.fullName())
+                .email(userCreateRequest.email())
+                .password(userCreateRequest.password())
+                .isActive(userCreateRequest.isActive()).build();
+        return userRepository.save(createdUser);
     }
 
     @Override
     public User updateUser(UUID id, User user) throws UserNotFoundException {
-        return null;
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException("User not found with id: "+id));
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setActive(user.isActive());
+        return userRepository.save(existingUser);
     }
 
     @Override
-    public List<User> getUsers() {
-        return List.of();
+    @Transactional(readOnly = true)
+    public Page<UserDto> getUsers(int page, int size) {
+        return userRepository.getUsers(PageRequest.of(page,size));
     }
 
     @Override
     public User findUserById(UUID id) throws UserNotFoundException {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException("User not found with id: "+ id));
     }
 
     @Override
     public void deleteUser(UUID id) {
-
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException("User not found with id: " + id));
+        existingUser.setActive(false);
+        userRepository.save(existingUser);
     }
 }
