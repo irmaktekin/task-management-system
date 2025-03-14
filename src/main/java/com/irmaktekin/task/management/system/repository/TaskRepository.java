c3f0c307e7915fc6bfb8ddbeee06d8fc895c9d67
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 00:55:57 +0300

    Create TaskRepository interface

---
+++ src/main/java/com/irmaktekin/task/management/system/repository/TaskRepository.java
@@ -0,0 +1,22 @@
+package com.irmaktekin.task.management.system.repository;
+
+import com.irmaktekin.task.management.system.dto.response.TaskDto;
+import com.irmaktekin.task.management.system.dto.response.UserDto;
+import com.irmaktekin.task.management.system.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    @Query("""
+            Select new com.irmaktekin.task.management.system.dto.response.TaskDto(
+                u.id, u.fullName, u.email, u.password,u.isActive)
+            From Task u
+            """)
+    Page<TaskDto> getTasks(Pageable pageable);
+}
