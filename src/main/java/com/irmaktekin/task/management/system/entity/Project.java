1a5b8f3f1d9b1c07d0666cdd11b1181a21324b65
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 01:24:50 +0300

    Add Project entity

--- src/main/java/com/irmaktekin/task/management/system/entity/Project.java
+++ src/main/java/com/irmaktekin/task/management/system/entity/Project.java
@@ -1,23 +1,38 @@
 package com.irmaktekin.task.management.system.entity;
 
-import com.irmaktekin.task.management.system.common.ProjectStatus;
+import com.irmaktekin.task.management.system.enums.ProjectStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private ProjectStatus status;
 
-    private String departmentName;
-    private String description;
-    private String title;
-    private List<User> members;
+    @Column(nullable = false)
+    private String responsibleDepartmentName;
 
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String title;
+
+    @OneToMany
+    private List<User> members;
 }
