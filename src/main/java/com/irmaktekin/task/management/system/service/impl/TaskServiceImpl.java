467b07e78101a1326326c1d33086887811cb5cfc
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 00:57:32 +0300

    Implement TaskService methods for get,create and update tasks

--- src/main/java/com/irmaktekin/task/management/system/service/impl/TaskServiceImpl.java
+++ src/main/java/com/irmaktekin/task/management/system/service/impl/TaskServiceImpl.java
@@ -1,34 +1,63 @@
 package com.irmaktekin.task.management.system.service.impl;
 
 import com.irmaktekin.task.management.system.common.exception.TaskNotFoundException;
+import com.irmaktekin.task.management.system.common.exception.UserNotFoundException;
 import com.irmaktekin.task.management.system.dto.request.TaskCreateRequest;
+import com.irmaktekin.task.management.system.dto.request.UserCreateRequest;
 import com.irmaktekin.task.management.system.dto.response.TaskDto;
+import com.irmaktekin.task.management.system.dto.response.UserDto;
 import com.irmaktekin.task.management.system.entity.Task;
+import com.irmaktekin.task.management.system.entity.User;
+import com.irmaktekin.task.management.system.repository.TaskRepository;
+import com.irmaktekin.task.management.system.repository.UserRepository;
 import com.irmaktekin.task.management.system.service.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @Override
-    public Task createTask(TaskCreateRequest taskCreateRequest) {
-        return null;
+    public Task createTask(@Valid TaskCreateRequest taskCreateRequest) {
+        Task createdTask = Task.builder().userStoryDescription(taskCreateRequest.description())
+                .taskPriority(taskCreateRequest.taskPriority())
+                .taskState(taskCreateRequest.taskState())
+                .acceptanceCriteria(taskCreateRequest.acceptanceCriteria())
+                .userStoryDescription(taskCreateRequest.description())
+                .build();
+        return taskRepository.save(createdTask);
     }
 
     @Override
     public Task updateTask(Task task) throws TaskNotFoundException {
-        return null;
+        Task existingTask = taskRepository.findById(task.getId())
+                .orElseThrow(()->new TaskNotFoundException("Task not found with id: "+task.getId()));
+        existingTask.setTaskPriority(task.getTaskPriority());
+        existingTask.setAssignee(task.getAssignee());
+        existingTask.setTaskPriority(task.getTaskPriority());
+        existingTask.setAcceptanceCriteria(task.getAcceptanceCriteria());
+        return taskRepository.save(existingTask);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Page<TaskDto> getTasks(int page, int size) {
-        return null;
+        return taskRepository.getTasks(PageRequest.of(page,size));
     }
 
     @Override
     public Task findTaskById(UUID id) throws TaskNotFoundException {
-        return null;
+        return taskRepository.findById(id)
+                .orElseThrow(()->new TaskNotFoundException("Task not found with id: "+ id));
     }
 }
