bf832990ee45e738758d7af884a3c685e872b6a0
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 23:30:59 +0300

    Create Role entity

---
+++ src/main/java/com/irmaktekin/task/management/system/entity/Role.java
@@ -0,0 +1,30 @@
+package com.irmaktekin.task.management.system.entity;
+
+import com.irmaktekin.task.management.system.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,unique = true)
+    private RoleType roleType;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
