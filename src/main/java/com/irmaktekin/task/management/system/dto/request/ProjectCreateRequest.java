7f3d8c85db390bb3a7912712eda03097cd5d060c
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 14:46:04 +0300

    Refactor memberId type from String to UUID

--- src/main/java/com/irmaktekin/task/management/system/dto/request/ProjectCreateRequest.java
+++ src/main/java/com/irmaktekin/task/management/system/dto/request/ProjectCreateRequest.java
@@ -3,9 +3,10 @@ package com.irmaktekin.task.management.system.dto.request;
 import com.irmaktekin.task.management.system.enums.ProjectStatus;
 
 import java.util.List;
+import java.util.UUID;
 
 public record ProjectCreateRequest(ProjectStatus projectStatus,
                                    String responsibleDepartmentName,
                                    String description,
                                    String title,
-                                   List<String> memberIds) {}
+                                   List<UUID> memberIds) {}
