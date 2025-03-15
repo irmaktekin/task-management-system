8ab42cfaec942dac971dbf72188886c35632187e
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 14:30:11 +0300

    Implement ProjectService methods

--- src/main/java/com/irmaktekin/task/management/system/service/ProjectService.java
+++ src/main/java/com/irmaktekin/task/management/system/service/ProjectService.java
@@ -5,12 +5,13 @@ import com.irmaktekin.task.management.system.dto.request.ProjectCreateRequest;
 import com.irmaktekin.task.management.system.dto.response.ProjectDto;
 import com.irmaktekin.task.management.system.entity.Project;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface ProjectService {
     Project createProject(ProjectCreateRequest projectCreateRequest);
     Project updateProject(Project project) throws ProjectNotFoundException;
-    Page<ProjectDto> getProjects(int page, int size);
+    Page<ProjectDto> getProjects(Pageable pageable);
     Project findProjectById(UUID id) throws ProjectNotFoundException;
 }
--- src/main/java/com/irmaktekin/task/management/system/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/irmaktekin/task/management/system/service/impl/ProjectServiceImpl.java
@@ -1,34 +1,65 @@
 package com.irmaktekin.task.management.system.service.impl;
 
 import com.irmaktekin.task.management.system.common.exception.ProjectNotFoundException;
+import com.irmaktekin.task.management.system.common.exception.UserNotFoundException;
 import com.irmaktekin.task.management.system.dto.request.ProjectCreateRequest;
 import com.irmaktekin.task.management.system.dto.response.ProjectDto;
 import com.irmaktekin.task.management.system.entity.Project;
+import com.irmaktekin.task.management.system.entity.User;
+import com.irmaktekin.task.management.system.repository.ProjectRepository;
+import com.irmaktekin.task.management.system.repository.UserRepository;
 import com.irmaktekin.task.management.system.service.ProjectService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
     @Override
     public Project createProject(ProjectCreateRequest projectCreateRequest) {
-        return null;
+        Project createdProject = Project.builder().responsibleDepartmentName(projectCreateRequest.responsibleDepartmentName())
+                .description(projectCreateRequest.description())
+                .status(projectCreateRequest.projectStatus()).build();
+        return projectRepository.save(createdProject);
     }
 
     @Override
     public Project updateProject(Project project) throws ProjectNotFoundException {
-        return null;
+        Project existingProject = projectRepository.findById(project.getId())
+                .orElseThrow(()->new UserNotFoundException("Project not found with id: "+project.getId()));
+        existingProject.setDescription(project.getDescription());
+        existingProject.setTitle(project.getTitle());
+        existingProject.setStatus(project.getStatus());
+        existingProject.setResponsibleDepartmentName(project.getResponsibleDepartmentName());
+        return projectRepository.save(existingProject);
     }
 
     @Override
-    public Page<ProjectDto> getProjects(int page, int size) {
-        return null;
+    public Page<ProjectDto> getProjects(Pageable pageable) {
+        return Optional.ofNullable(projectRepository.getProjects(pageable))
+                .orElse(Page.empty())
+                .map(project -> new ProjectDto(
+                        project.getId(),
+                        project.getStatus(),
+                        project.getResponsibleDepartmentName(),
+                        project.getDescription(),
+                        project.getTitle(),
+                        project.getMembers().stream().map(User::getId).toList()));
     }
 
     @Override
     public Project findProjectById(UUID id) throws ProjectNotFoundException {
-        return null;
+        return projectRepository.findById(id)
+                .orElseThrow(()->new ProjectNotFoundException("Project not found with id: "+ id));
     }
 }
