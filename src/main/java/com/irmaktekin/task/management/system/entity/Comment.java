d8487468bee4886369ab708e9bc5ee5c6875a492
irmaktekin <irmaktekin97@gmail.com>
2025-03-16 00:12:04 +0300

    Add Comment entity

--- src/main/java/com/irmaktekin/task/management/system/entity/Comment.java
+++ src/main/java/com/irmaktekin/task/management/system/entity/Comment.java
@@ -1,18 +1,29 @@
 package com.irmaktekin.task.management.system.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
     private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User commentOwnerName;
 }
