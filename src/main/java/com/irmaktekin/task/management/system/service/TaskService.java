e062a5ddc5dcfcbe679a8af151b8c345accea60b
irmaktekin <irmaktekin97@gmail.com>
2025-03-15 14:53:43 +0300

    Refactor getTasks method parameter as Pageable

--- src/main/java/com/irmaktekin/task/management/system/service/TaskService.java
+++ src/main/java/com/irmaktekin/task/management/system/service/TaskService.java
@@ -5,12 +5,13 @@ import com.irmaktekin.task.management.system.dto.request.TaskCreateRequest;
 import com.irmaktekin.task.management.system.dto.response.TaskDto;
 import com.irmaktekin.task.management.system.entity.Task;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface TaskService {
     Task createTask(TaskCreateRequest taskCreateRequest);
     Task updateTask(Task task) throws TaskNotFoundException;
-    Page<TaskDto> getTasks(int page, int size);
+    Page<TaskDto> getTasks(Pageable pageable);
     Task findTaskById(UUID id) throws TaskNotFoundException;
 }
