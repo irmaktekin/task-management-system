bd6835adad31427f8202369824489a6b45060d17
irmaktekin <irmaktekin97@gmail.com>
2025-03-17 22:57:12 +0300

    Add Builder annotation to Attachment entity

--- src/main/java/com/irmaktekin/task/management/system/entity/Attachment.java
+++ src/main/java/com/irmaktekin/task/management/system/entity/Attachment.java
@@ -2,6 +2,7 @@ package com.irmaktekin.task.management.system.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
