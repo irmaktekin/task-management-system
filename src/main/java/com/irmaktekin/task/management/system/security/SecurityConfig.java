39611aafff36b9244eab40b49f39c46853307226
irmaktekin <irmaktekin97@gmail.com>
2025-03-21 01:59:07 +0300

    Refactor Security configurations for authorization

--- src/main/java/com/irmaktekin/task/management/system/security/SecurityConfig.java
+++ src/main/java/com/irmaktekin/task/management/system/security/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.irmaktekin.task.management.system.security;
 
+import com.irmaktekin.task.management.system.controller.TaskController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtFilter jwtFilter;
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
     public SecurityConfig(UserDetailsService userDetailsService, JwtFilter jwtFilter) {
         this.userDetailsService = userDetailsService;
@@ -38,12 +43,17 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.debug("Configuring Security Filter Chain...");
+
         http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers("/api/v1/auth").permitAll().
-                    anyRequest().permitAll(); ;
-                })
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/tasks/{taskId}/assignee/{assigneeId}").hasAnyRole("PROJECT_MANAGER","TEAM_LEADER")
+                        .requestMatchers(HttpMethod.GET,"/api/v1/tasks/{taskId}/status").hasAnyRole("PROJECT_MANAGER","TEAM_LEADER")
+                        .requestMatchers( "/api/v1/projects/**").hasRole("PROJECT_MANAGER")
+                        .anyRequest().authenticated()
+        )
+
                 .formLogin(AbstractAuthenticationFilterConfigurer::disable)
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
